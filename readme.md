0fdb7f10ba6938a2a78eb27ab55db5de233368fc
pearpages <pere@soms.cat>
2016-11-29 22:29:56 +0100

    merge

--- readme.md
+++ readme.md
@@ -60,3 +60,27 @@ const startIntervalThatStopsAndRestarts$ = start$.switchMapTo(intervalThatStops$
     }, { count: 0 })
     .subscribe((x) => console.log(x));
 ```
+
+---
+
+## merge
+
+```javascript
+const startButton = document.querySelector('#start');
+const stopButton = document.querySelector('#stop');
+const resetButton = document.querySelector('#reset');
+
+const start$ = Observable.fromEvent(startButton, 'click');
+const stop$ = Observable.fromEvent(stopButton, 'click');
+const reset$ = Observable.fromEvent(resetButton,'click');
+const interval$ = Observable.interval(1000);
+const intervalThatStops$ = interval$.takeUntil(stop$);
+
+const inc = (acc) => ({ count: acc.count + 1 });
+const reset = (acc) => ({ count: 0});
+const incOrReset$ =  Observable.merge(intervalThatStops$.mapTo(inc),reset$.mapTo(reset));
+
+const startIntervalThatStopsAndRestarts$ = start$.switchMapTo(incOrReset$)
+    .scan( (acc,curr) => {return curr(acc);})
+    .subscribe((x) => console.log(x));
+```
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,15 +16,20 @@ export class AppComponent {
   ngOnInit() {
     const startButton = document.querySelector('#start');
     const stopButton = document.querySelector('#stop');
+    const resetButton = document.querySelector('#reset');
 
     const start$ = Observable.fromEvent(startButton, 'click');
     const stop$ = Observable.fromEvent(stopButton, 'click');
+    const reset$ = Observable.fromEvent(resetButton,'click');
     const interval$ = Observable.interval(1000);
     const intervalThatStops$ = interval$.takeUntil(stop$);
-    const startIntervalThatStopsAndRestarts$ = start$.switchMapTo(intervalThatStops$)
-      .scan( (acc) => {
-        return { count: acc.count + 1 }
-      }, { count: 0 })
+
+    const inc = (acc) => ({ count: acc.count + 1 });
+    const reset = (acc) => ({ count: 0});
+    const incOrReset$ =  Observable.merge(intervalThatStops$.mapTo(inc),reset$.mapTo(reset));
+
+    const startIntervalThatStopsAndRestarts$ = start$.switchMapTo(incOrReset$)
+      .scan( (acc,curr) => {return curr(acc);})
       .subscribe((x) => console.log(x));
       
   }
